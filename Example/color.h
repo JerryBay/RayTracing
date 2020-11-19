23187d6507a5c740e456fbad6173e2efbce3829b
jerryjcbai <1018679568@qq.com>
2020-11-19 21:35:24 +0800

    fix bugs that grey picture

--- Example/color.h
+++ Example/color.h
@@ -6,9 +6,9 @@ using color = vec3;    // RGB color
 
 void write_color(std::ostream& out, color pixel_color,int samples_per_pixel)
 {
-	double r = pixel_color.x();
-	double g = pixel_color.y();
-	double b = pixel_color.z();
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
 
 	// Divide the color by the number of samples.
 	double scale = 1.0 / samples_per_pixel;
@@ -18,6 +18,6 @@ void write_color(std::ostream& out, color pixel_color,int samples_per_pixel)
 
 	// Write the translated [0,255] value of each color component.
 	out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
-		<< static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
-		<< static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << '\n';
+		<< static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
+		<< static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
 }
--- Example/utils.h
+++ Example/utils.h
@@ -15,16 +15,10 @@ inline double degrees_to_radians(double degrees)
 	return degrees * pi / 180.0;
 }
 
-inline double clamp(double x, double min, double max)
-{
-	if (x<min)
-	{
-		return min;
-	}
-	if (x>max)
-	{
-		return max;
-	}
+inline double clamp(double x, double min, double max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
 }
 
 inline double random_double()
