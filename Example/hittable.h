326556a0bb493b81d6ee9c5cc557e6a556712bc7
jerryjcbai <1018679568@qq.com>
2020-11-29 21:52:46 +0800

    add material attribution

--- Example/hittable.h
+++ Example/hittable.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "ray.h"
 
+class material;
+
 struct hit_record
 {
 	point3 p;
 	vec3 normal;
 	double t;
 	bool front_face;
+	shared_ptr<material> mat_ptr;
 
 	inline void set_face_normal(const ray& r, const vec3& outward_normal)
 	{
--- Example/vec3.h
+++ Example/vec3.h
@@ -53,6 +53,13 @@ public:
         return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 
+    bool near_zero()const
+    {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1] < s)) && (fabs(e[2]) < s);
+    }
+
 public:
     double e[3];
 };
@@ -114,4 +121,27 @@ vec3 random_in_unit_sphere()
         }
         return p;
     }
+}
+
+vec3 random_unit_vector()
+{
+    return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3& normal)
+{
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere,normal)>0.0)
+    {
+        return in_unit_sphere;
+    }
+    else 
+    {
+        return -in_unit_sphere;
+    }
+}
+
+vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2 * dot(v, n) * n;
 }
