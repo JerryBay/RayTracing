7b46a167f898bac46505643cdf21e2a98f0951b7
jerryjcbai <1018679568@qq.com>
2020-11-30 21:17:34 +0800

    refraction material

--- Example/main.cpp
+++ Example/main.cpp
@@ -13,11 +13,11 @@ color ray_color(const ray& r, const hittable& world,int depth) {
         return color(0, 0, 0);
     }
 
-    if (world.hit(r, 0.001, infinity, rec)) {
+    if (world.hit(r, 0.001, infinity, rec)) 
+    {
         ray scattered;
         color attenuation;
-        if (rec.mat_ptr->scatter(r,rec,attenuation,scattered))
-        {
+        if (rec.mat_ptr->scatter(r,rec,attenuation,scattered)){
             return attenuation * ray_color(scattered, world, depth - 1);
         }
         return color(0, 0, 0);
@@ -43,13 +43,14 @@ int main() {
 
     hittable_list world;
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
